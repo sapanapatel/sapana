3ad35141771866c0fd13d720379d442b1577c043
Sapana Patel <spp7@njit.edu>
2023-09-12 11:32:08 +0200

    python module for Raspberry

--- temp.py
+++ temp.py
@@ -1,8 +1,11 @@
 #! /usr/bin/python
 TEMP_PATH="/sys/class/thermal/thermal_zone0/"
 
-fo = open (TEMP_PATH + "temp", "r")
-temp = fo.read()
-print(temp)
+#fo = open (TEMP_PATH + "temp", "r")
+#temp = fo.read()
+#print(temp)
 
-
+def readTemp():
+    fo = open(TEMP_PATH + "temp", "r")
+    temp = fo.read()
+    return temp
